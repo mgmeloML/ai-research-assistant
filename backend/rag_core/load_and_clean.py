d0eba59ff82456e8c102f85e8a1f005749210961
Mayowa Ogundele (k23004722) <profmayowa@gmail.com>
2025-09-03 22:33:44 +0100

    add loading and cleaning of documents

--- backend/rag_core/load_and_clean.py
+++ backend/rag_core/load_and_clean.py
@@ -1,28 +1,68 @@
-from langchain_community.document_loaders import PyPDFDirectoryLoader
-import os
-import re
+from langchain_community.document_loaders import PyPDFDirectoryLoader, DirectoryLoader, TextLoader
+import os,re, json, html
 from dotenv import load_dotenv
 
 load_dotenv()
 
+directory = os.getenv("PDF_DIR")
+folder = os.getenv("SCRAPE_DIR")
+
 def clean_pdf_text(text):
-    # Fix line breaks
-    text = re.sub(r'-\s*\n\s*', '', text)  # fix hyphenation
-    text = re.sub(r'(?<!\n)\n(?!\n)', ' ', text)  # join lines
-    # Remove multiple spaces
+    text = re.sub(r'-\s*\n\s*', '', text) 
+    text = re.sub(r'(?<!\n)\n(?!\n)', ' ', text) 
     text = re.sub(r'\s+', ' ', text)
-    # Remove page numbers
     text = re.sub(r'\n\d+\n', '\n', text)
-    # Optional: remove references
     text = re.split(r'\nReferences\n', text, flags=re.IGNORECASE)[0]
     return text.strip()
 
-dir_path = os.getenv("PDF_DIR")
+def clean_scraped_text(text):
+    text = re.sub(r"\[\d+\]", "", text)  
+    text = re.sub(r"<.*?>", "", text)
+    text = re.sub(r"<script.*?>.*?</script>", "", text, flags=re.DOTALL | re.IGNORECASE)
+    text = re.sub(r"<style.*?>.*?</style>", "", text, flags=re.DOTALL | re.IGNORECASE)
+    text = re.sub(r"\([^)]*adsbygoogle[^)]*\)", "", text, flags=re.IGNORECASE)
+    text = re.sub(r"\([^)]*click here[^)]*\)", "", text, flags=re.IGNORECASE)
+    text = re.sub(r"\([^)]*read more[^)]*\)", "", text, flags=re.IGNORECASE)
+    text = re.sub(r"&[a-z]+;", " ", text)
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
 
-loader = PyPDFDirectoryLoader(dir_path)
-docs = loader.load()
+def resolve_metadata(doc , directory):
+    meta = doc.metadata["source"]
+    meta = meta.split("\\")[-1]
+    meta = meta[:-4]
 
-print(len(docs[0].page_content))
-for doc in docs:
-    doc.page_content = clean_pdf_text(doc.page_content)
+    with open(directory+"/"+meta+".json", "r") as f:
+        metadata = json.load(f)
 
+    title = metadata["title"]
+    url = metadata["url"]
+    doc.metadata["source"] = url
+    doc.metadata["title"] = title
+    
+    return doc
+
+
+def load_pdf_docs(directory):
+    loader = PyPDFDirectoryLoader(directory)
+    docs = loader.load()
+
+    for doc in docs:
+        doc.page_content = clean_pdf_text(doc.page_content)
+        doc = resolve_metadata(doc=doc, directory=directory)
+
+    return docs
+
+def load_scraped_docs(directory):
+    text_loader_kwargs = {"autodetect_encoding": True}
+    loader = DirectoryLoader(directory, glob="**/*.txt", loader_cls=TextLoader, silent_errors=True, loader_kwargs = text_loader_kwargs)
+    docs = loader.load()
+
+    for doc in docs:
+        doc.page_content = clean_scraped_text(doc.page_content)
+        doc = resolve_metadata(doc=doc, directory=directory)
+
+    return docs
+
+if __name__ == "__main__":
+    print(load_pdf_docs(directory)[2])
