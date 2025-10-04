fea68c1cec75b7589fc6959f8008a5e2b5940fd6
Mayowa Ogundele (k23004722) <profmayowa@gmail.com>
2025-10-04 02:22:51 +0100

    Refactor pipeline.py to enhance data collection by dynamically determining max results and using generated collection names for PDF and web data processing

--- backend/rag_core/pipeline.py
+++ backend/rag_core/pipeline.py
@@ -1,13 +1,13 @@
-import os
+import os, glob
 from dotenv import load_dotenv
 from rag_core.data_collection import collect_pdf_data, collect_web_data
 from rag_core.load_and_clean import load_pdf_docs, load_scraped_docs
 from rag_core.chunk_docs import split_text
 from rag_core.build_index import make_database, get_embedding_function, retrieve_docs
-from rag_core.generate import generate_response
+from rag_core.generate import generate_response, generate_chat_name, generate_collection_name, num_sources
 
 
-load_dotenv()#
+load_dotenv()
 
 embeddings = get_embedding_function()
 scraped_dir = os.getenv("SCRAPE_DIR")
@@ -18,28 +18,34 @@ vector_path = os.getenv("VECTOR_DIR")
 
 def rag_pdf():
     query = str(input("your input:"))
-    collect_pdf_data(query=query, directory=pdf_dir, max_results=5)
+    max_results = int(num_sources(query))
+    collect_pdf_data(query=query, directory=pdf_dir, max_results=max_results)
     documents = load_pdf_docs(pdf_dir)
     chunks = split_text(documents=documents)
     single_vector = embeddings.embed_query(query)
-
-    make_database(chunks, collection_name="pdf")
-    docs = retrieve_docs(single_vector, collection_name="pdf")
+    collection_name = generate_collection_name(query)
+    make_database(chunks, collection_name=collection_name)
+    docs = retrieve_docs(single_vector, collection_name=collection_name)
     generate_response(docs, query)
 
 def rag_web():
     query = str(input("your input:"))
-    collect_web_data(query=query, directory=scraped_dir, max_results=5)
+    max_results = int(num_sources(query))
+    collect_web_data(query=query, directory=scraped_dir, max_results=max_results)
     documents = load_scraped_docs(scraped_dir)
     chunks = split_text(documents=documents)
     single_vector = embeddings.embed_query(query)
-
-    make_database(chunks, collection_name="web")
-    docs = retrieve_docs(single_vector, collection_name="web")
+    collection_name = generate_collection_name(query)
+    make_database(chunks, collection_name=collection_name)
+    docs = retrieve_docs(single_vector, collection_name=collection_name)
     generate_response(docs, query)
+
 
 flow = str(input("do you want web or pdf pipeline?:"))
 if flow == "web":
     rag_web()
 elif flow == "pdf":
     rag_pdf()
+
+[os.remove(f) for f in glob.glob(f"{scraped_dir}*") if os.path.isfile(f)]
+[os.remove(f) for f in glob.glob(f"{pdf_dir}*") if os.path.isfile(f)]
